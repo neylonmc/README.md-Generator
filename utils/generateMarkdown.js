09af264d93af7754358ce608d06faa12d4a6ccc2
Meaghan Neylon <mneylon@richmond.edu>
2020-10-19 11:48:45 -0400

    Added function to dynamically create a markdown file based on the users answers.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,35 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
+// Function to generate markdown for README
+function generateMarkdown(answers) {
+  return `
+  # ${answers.title}
+  ### Description
+  ${answers.description}
+  ## Table of Contents
+  \n* [Installation](#installation)
+  \n* [Usage](#usage)
+  \n* [Credits](#credits)
+  \n* [License](#license)
+      
+  ## Installation
+  \`\`\`
+  ${answers.installation}
+  \`\`\`
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## License
+  [![License](https://img.shields.io/badge/License-${answers.license}%202.0-blue.svg)](https://opensource.org/licenses/${answers.license})
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.test}
+  
+  ## Questions? Contact Me
+  GitHub: [${answers.github}]("https://github.com/${answers.github}")
+  Email: [${answers.email}]("mailto:${answers.email}") `
+};
 
 module.exports = generateMarkdown;
